66823f8d96a9b2996612dd470899aac07c04d755
Jonas Bernoulli <jonas@bernoul.li>
2024-12-26 17:55:28 +0100

    Move metadata from roguel-ike-pkg.el to roguel-ike.el

--- roguel-ike-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "roguel-ike" "0.1.0" "A coffee-break roguelike"
-  '((popup "0.5.0")))
--- roguel-ike.el
+++ roguel-ike.el
@@ -2,9 +2,11 @@
 
 ;; Copyright (C) 2014 Steven Rémot
 
-;;; Author: Steven Rémot
+;; Author: Steven Rémot
+;; Package-Requires: ((popup "0.5.0"))
 
 ;;; License:
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
